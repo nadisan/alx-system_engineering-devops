ea2455449b6f29c3167ce1993c1a978709902cc6
Nadia <nadiawellela@gmail.com>
2023-09-22 09:22:49 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # create a manifest that kills a process named killmenow.
+
 exec{'killmenow':
 command => 'pkill -f killmenow',
-onlyif  => 'pgrep -f killmenow',
+onlyif  => 'pgrep -f killmenow'
 }
