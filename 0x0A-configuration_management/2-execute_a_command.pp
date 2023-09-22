97fa96c6169bacdac9786559978c48d8cbd7873d
Nadia <nadiawellela@gmail.com>
2023-09-22 09:18:32 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow.
 exec{'killmenow':
-command => 'pkill -f my_process.py',
-onlyif  => 'pgrep -f my_process.py',
+command => 'pkill -f killmenow',
+onlyif  => 'pgrep -f killmenow',
 }
