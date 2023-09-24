c11364076084220396c7b15f42c69bd4f8fb3a53
Nadia <nadiawellela@gmail.com>
2023-09-24 11:21:14 -0700

    rm

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,5 +0,0 @@
-# puppet ssh configuration
-exec { 'ssh_config':
-  path    => '/bin',
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-}
