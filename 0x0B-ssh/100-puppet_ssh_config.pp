37bede296cb9b7b5d7269e940381d23b1867c191
Nadia <nadiawellela@gmail.com>
2023-09-24 19:42:14 -0700

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # SSH config puppet
-exec {'ssh_congig':
+exec {'ssh_config':
        path    => '/bin',
        command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentifyFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
