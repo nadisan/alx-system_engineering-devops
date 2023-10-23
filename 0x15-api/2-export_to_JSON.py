6c8d679c82bfd74ae7b6de7cd39ffedb44a1b02a
nadisan <nadiawellela@gmail.com>
2023-10-23 15:51:47 -0700

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" Python script that exports data in the CSV format """
+from requests import get
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            employee = i['username']
+            empid = i['id']
+
+    row = []
+
+
+    for i in data:
+        new_dict = {}
+        if i['userId'] == int(argv[1]):
+            new_dict['username'] = employee
+            new_dict['task'] = i['title']
+            new_dict['completed'] = i['completed']
+            row.append(new_dict)
+    final_dict = {}
+    final_dict[empid] = row
+    json_obj = json.dumps(final_dict)
+
+    with open((argv[1]) + ".json", "w") as f:
+        f.write(json_obj)
