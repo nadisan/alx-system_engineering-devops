10145af937ee48d9e4c05296cb7a9ce4b4d9b83b
nadisan <nadiawellela@gmail.com>
2023-10-23 10:22:49 -0700

    tasko

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
-"""
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+"""  given employee ID,returns information about his/her TODO list progress"""
 
 from requests import get
 from sys import argv
-
 
 if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
